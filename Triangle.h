bcbcdda53fdcf6887cdad4dcd4e9ab447d8a05d6
Mamoonalatif <151760913+Mamoonalatif@users.noreply.github.com>
2024-05-19 20:10:50 +0500

    Update Triangle.h

--- Triangle.h
+++ Triangle.h
@@ -1,18 +1,22 @@
 #pragma once
 #include "Gems.h"
+#include <cmath>
 class Triangle : public Gems {
 private:
-	int sidelength = 50;
+    int sidelength = 30;
 public:
-	void draw() override{
-		glColor3f(1.0, 0.00, 0.00);
-		glBegin(GL_TRIANGLES);
-		glVertex2f(x, y);
-		glVertex2f(x + sidelength, y);
-		glVertex2f(x + sidelength / 2, y + (sidelength * sqrt(3)) / 2);
-		glEnd();
-	}
-	void move() override {
-		x = 0;
-	}
+    Triangle(float a, float b) : Gems(a, b, "Triangle") {
+        
+    }
+    void draw() override {
+        glColor3f(1.0, 0.00, 0.00); 
+        glBegin(GL_TRIANGLES);
+        glVertex2f(x, y);
+        glVertex2f(x + sidelength, y);
+        glVertex2f(x + sidelength / 2, y + (sidelength * sqrt(3)) / 2);
+        glEnd();
+    }
+    void move() override {
+        x = 0; 
+    }
 };
