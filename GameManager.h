6ee8c358684f0b5259e8248865b8059fc50c1041
Mamoonalatif <151760913+Mamoonalatif@users.noreply.github.com>
2024-05-26 14:12:28 +0500

    Update GameManager.h

--- GameManager.h
+++ GameManager.h
@@ -1,1 +1,20 @@
+#include"GameBoard.h"
+class GameManager {
+public:
+    bool mainMenuState;
+    bool pauseState;
+    bool endState;
+    bool playState;
+    GameBoard* board;
+    GameManager(GameBoard* board) : mainMenuState(true), pauseState(false), endState(false), playState(false), board(board) {}
+    void setMainMenuState(bool state) { mainMenuState = state; }
+    bool getMainMenuState() const { return mainMenuState; }
+    void setPauseState(bool state) { pauseState = state; }
+    bool getPauseState() const { return pauseState; }
+    void setEndState(bool state) { endState = state; }
+    bool getEndState() const { return endState; }
+    void setPlayState(bool state) { playState = state; }
+    bool getPlayState() const { return playState; }
+    bool checkEnd() const { return endState; }
+};
 
