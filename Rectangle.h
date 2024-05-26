3193c1b167b4a1f605d350b1b12f65e5b30bd34b
Mamoonalatif <151760913+Mamoonalatif@users.noreply.github.com>
2024-05-26 14:13:56 +0500

    Update Rectangle.h

--- Rectangle.h
+++ Rectangle.h
@@ -1,1 +1,37 @@
-#pragma once
+#include "Gems.h"
+#include <GL/glut.h>
+
+class Rectangle : public Gems {
+private:
+    float width; 
+    float height; 
+    float color[3]; s
+public:
+    Rectangle(float a, float b, float w, float h) : Gems("Rectangle"), width(w), height(h) {
+        x = a;
+        y = b;
+        
+        color[0] = 0.0f;
+        color[1] = 1.0f; 
+        color[2] = 0.0f;
+    }
+
+    void draw() override {
+        glPushMatrix();
+        glTranslatef(x, y, 0.0f); 
+        glColor3fv(color); 
+        glBegin(GL_POLYGON);
+        
+        glVertex2f(-width / 2.0f, -height / 2.0f); 
+        glVertex2f(width / 2.0f, -height / 2.0f); 
+        glVertex2f(width / 2.0f, height / 2.0f); 
+        glVertex2f(-width / 2.0f, height / 2.0f); 
+        glEnd();
+        glPopMatrix();
+    }
+
+    void move() override {
+        // Implement movement logic for rectangle if needed
+    }
+};
+
